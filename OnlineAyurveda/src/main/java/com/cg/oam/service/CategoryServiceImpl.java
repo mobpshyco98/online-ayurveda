9fa1d6b078012177bb584e93e7231e7e8451ae06
ARGHYA <ARGHYA@DESKTOP-LSDF607>
2021-05-18 20:38:01 +0530

    commit2Arghya

---
+++ OnlineAyurveda/src/main/java/com/cg/oam/service/CategoryServiceImpl.java
@@ -0,0 +1,41 @@
+package com.cg.oam.service;
+
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.cg.oam.dao.ICategoryDao;
+import com.cg.oam.dto.CategoryDto;
+import com.cg.oam.entities.Category;
+import com.cg.oam.exceptions.CategoryNotFoundException;
+
+@Service
+@Transactional
+
+public class CategoryServiceImpl implements ICategoryService{
+	
+	@Autowired
+	private ICategoryDao categoryDao;
+
+	@Override
+	public Integer addCategory(CategoryDto categorydto) {
+		Category category=new Category();
+		category.setCategoryName(categorydto.getCategoryName());
+		Category newCategory= categoryDao.save(category);
+		return newCategory.getCategoryId();
+			
+	
+	}
+
+	@Override
+	public List<Category> viewAllCategory() {
+		List<Category> categoryList=categoryDao.findAll();
+		
+		return categoryList;
+	}
+	
+
+}
