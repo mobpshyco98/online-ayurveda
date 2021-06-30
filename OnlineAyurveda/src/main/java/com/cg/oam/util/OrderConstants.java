51ebbe83aec3de70eaf6f63ba4607fe95c7b4034
AkashGaurav15 <gaurav.prasad.kol@gmail.com>
2021-06-30 21:04:13 +0530

    Gaurav_Commit_!!

--- OnlineAyurveda/src/main/java/com/cg/oam/util/OrderConstants.java
+++ OnlineAyurveda/src/main/java/com/cg/oam/util/OrderConstants.java
@@ -5,7 +5,7 @@ public class OrderConstants {
 	public static final String BASKET_EMPTY="basket is empty ";
 	public static final String ORDER_CONFIRMED="ordered";
 	public static final Integer DECREMENT_BY_ONE=1;
-	public static final String ORDER_CREATED="You have Ordered Succesfully Your Order Id is ";
+	public static final String ORDER_CREATED="You have Ordered Succesfully. Your Order Id is ";
 	public static final String ORDER_EMPTY="No Order Placed";
 	public static final String ORDER_NOT_FOUND="No Order Found";
 	public static final String REMOVE_BY_ORDER_ID = "removing all orders by order_id";
